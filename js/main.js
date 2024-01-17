6c29c00812b88ced7a32eda4ffd4403bec05566e
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-01-17 04:31:38 +0100

    feat: add if multiple of 15

--- js/main.js
+++ js/main.js
@@ -4,6 +4,13 @@ console.log('JS OK')
 // ciclo for da 1 a 100
 for (let i=1; i <= 100; i++) {
     
-    console.log(i)
+    // se il numero è multiplo di 15
+    if (i % 15 == 0) {
+        
+        // stampo fizzbuzz in console
+        console.log("FizzBuzz")
 
+    } 
+
+    console.log(i)
 }
